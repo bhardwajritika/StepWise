365ff8137084bb7d5600e19376d07acc52dd47ef
Ritika <bhardwajritika11@gmail.com>
2026-03-06 00:48:22 +0530

    Initial Commit

---
+++ StepWise/StepWiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  StepWiseApp.swift
+//  StepWise
+//
+//  Created by Tarun Sharma on 06/03/26.
+//
+
+import SwiftUI
+
+@main
+struct StepWiseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
