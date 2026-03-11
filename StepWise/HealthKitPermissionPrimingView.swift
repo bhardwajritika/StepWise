7a796dc337953ec2a58b8dd50ac6725a55905fee
Ritika <bhardwajritika11@gmail.com>
2026-03-11 19:23:54 +0530

    Healthkit UI permission view added

---
+++ StepWise/HealthKitPermissionPrimingView.swift
@@ -0,0 +1,44 @@
+//
+//  HealthKitPermissionPrimingView.swift
+//  StepWise
+//
+//  Created by Tarun Sharma on 11/03/26.
+//
+
+import SwiftUI
+
+struct HealthKitPermissionPrimingView: View {
+    
+    var description = """
+This app displays your steps and weight data in interactive charts.
+
+You can also add new steps and weight data to Apple Health through this app. Your data is private and secure.
+"""
+    var body: some View {
+        VStack (spacing: 130){
+            VStack (alignment: .leading, spacing: 10 ){
+                Image(.appleHealth)
+                    .resizable()
+                    .frame(width: 90, height: 90)
+                    .shadow(color: .gray.opacity(0.3),radius: 16)
+                    .padding(.bottom, 12)
+                
+                Text("Apple Health Intergration")
+                    .font(.title2).bold()
+                
+                Text(description)
+            }
+            
+            Button("Connect Apple Health") {
+                
+            }
+            .buttonStyle(.borderedProminent)
+            .tint(.pink)
+        }
+        .padding(30)
+    }
+}
+
+#Preview {
+    HealthKitPermissionPrimingView()
+}
