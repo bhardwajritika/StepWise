0a6bac50d604b03ab43244a69be028099aa8612e
Ritika <bhardwajritika11@gmail.com>
2026-03-20 01:59:27 +0530

    Pie chart interactivity

--- StepWise/Preview Content/Extensions/Date+Ext.swift
+++ StepWise/Preview Content/Extensions/Date+Ext.swift
@@ -11,4 +11,18 @@ extension Date {
     var weekdayInt: Int {
         Calendar.current.component(.weekday, from: self)
     }
+    
+    var weekdayTitle : String {
+        switch weekdayInt {
+        case 1: return "Sunday"
+        case 2: return "Monday"
+        case 3: return "Tuesday"
+        case 4: return "Wednesday"
+        case 5: return "Thursday"
+        case 6: return "Friday"
+        case 7: return "Saturday"
+        default:
+            return ""
+        }
+    }
 }
