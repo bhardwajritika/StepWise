379a4e8b62f2da778de5a65bcb1131bcb2418ca3
Ritika <bhardwajritika11@gmail.com>
2026-03-23 01:40:45 +0530

    implemented chart empty state

---
+++ StepWise/Preview Content/Charts/ChartEmptyView.swift
@@ -0,0 +1,34 @@
+//
+//  ChartEmptyView.swift
+//  StepWise
+//
+//  Created by Tarun Sharma on 23/03/26.
+//
+
+import SwiftUI
+
+struct ChartEmptyView: View {
+    let systemImageName: String
+    let title: String
+    let description: String
+    
+    var body: some View {
+        ContentUnavailableView {
+            Image(systemName: systemImageName)
+                .resizable()
+                .frame(width: 32, height: 32)
+                .padding(.bottom, 8)
+            Text(title)
+                .font(.callout.bold())
+            
+            Text(description)
+                .font(.footnote)
+        }
+        .foregroundStyle(.secondary)
+        .offset(y: -12)
+    }
+}
+
+#Preview {
+    ChartEmptyView(systemImageName: "chart.bar", title: "No Data", description: "There is no data from the Health App")
+}
